ffe9353b0d7501390a9be5a7db31f216ee504f77
ragulnathMB <ragulnath255@gmail.com>
2025-01-02 18:38:29 +0530

    modified ui of git tracker

--- src/Components/GitTracker/GitTracker.jsx
+++ src/Components/GitTracker/GitTracker.jsx
@@ -1,45 +1,56 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import styles from './GitTracker.module.css'; // Importing CSS Module
 
 const restEndpoint = "https://api.github.com/repos/ragulnathMB/hacker-dashboard/commits";
 
 const callRestApi = async () => {
     const response = await fetch(restEndpoint);
     const jsonResponse = await response.json();
-    console.log(jsonResponse);
-    // return JSON.stringify(jsonResponse);
 
+    // Create a styled list of commit records
     const arrayOfLists = jsonResponse.map(
-        record => <li key={record.commit.author.date}>author: {record.commit.author.name}, <br />message: "{record.commit.message}", <br />time: {record.commit.author.date}</li>
-    )
+        record => (
+            <li className={styles.commitItem} key={record.commit.author.date}>
+                <div className={styles.commitContent}>
+                    <div>
+                    <div><FontAwesomeIcon className={styles.bellIcon} icon={faBell} title="Notify about this activity" /></div>
+                        <strong>Author:</strong> {record.commit.author.name} <br />
+                        <strong>Message:</strong> "{record.commit.message}" <br />
+                        <strong>Time:</strong> {new Date(record.commit.author.date).toLocaleString()}
+                    </div>
+                </div>
+            </li>
+        )
+    );
     return arrayOfLists;
 };
 
-
-
-
 function RenderResult() {
-    const [apiResponse, setApiResponse] = useState("*** now loading ***");
+    const [apiResponse, setApiResponse] = useState(<p>*** now loading ***</p>);
 
     useEffect(() => {
-        callRestApi().then(
-            result => setApiResponse(result));
+        callRestApi().then(result => setApiResponse(result));
     }, []);
 
     return (
-        <div>
-            <p>{apiResponse}</p>
+        <div className={styles.resultsContainer}>
+            <ul className={styles.commitList}>{apiResponse}</ul>
         </div>
     );
-};
+}
 
 const GitTracker = () => {
     return (
-        <>
+        <div className={styles.gitTrackerContainer}>
+            <header className={styles.trackerHeader}>
+                <h1>GitHub RepoTracker</h1>
+            </header>
+            <div><p>Recent Activities</p></div>
             <RenderResult />
-        </>
-
-
-    )
+        </div>
+    );
 };
 
 export default GitTracker;
